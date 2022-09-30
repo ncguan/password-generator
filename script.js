1382aae33b4e00d13ad97907a0864fa0909168cc
NG <nicholeguan2@gmail.com>
2022-09-29 23:02:36 -0700

    add notes in js to explain some code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//function to get length of password
 function getLength(){
   var passLength = prompt("How many characters is your password? Pick a number from 8 to 128.");
   while (passLength <8 || passLength>128 || isNaN(passLength)==true){
@@ -6,10 +7,19 @@ function getLength(){
   return passLength
 }
 
+var arrLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var arrUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var arrNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var arrSpecial = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+
+//arrGenerate is an array of the character types
 var arrGenerate = [];
+//containType is used as a boolean array
 var containType = [];
+//function that gets user input of password character type criteria 
 function getTypes() {
   var charCheck = false
+  //repeats so that at least one character type is confirmed
   while (charCheck==false){
     alert("Confirm at least one character type in your password.");
     var passLower = confirm("Click OK to confirm that your password includes lowercase characters.");
@@ -19,6 +29,7 @@ function getTypes() {
     var charType = [passLower, passUpper, passNum, passSpecial];
     charChecker();
 
+    //function checks that there is at least one confirmed character type
     function charChecker(){
       if (charType.includes(true)){
         charCheck=true;
@@ -31,6 +42,7 @@ function getTypes() {
     }
   }
   
+  //if character type lowercase was confirmed, push array of lowercase charatcers into arrGenerate and containType[0]=true
   if (charType[0] == true){
     arrGenerate.push(arrLower);
     containType.push(passLower);
@@ -39,6 +51,7 @@ function getTypes() {
     containType.push(passLower);
   }
 
+  //if character type uppercase was confirmed, push array of uppercase charatcers into arrGenerate and containType[1]=true
   if (charType[1] == true){
     arrGenerate.push(arrUpper);
     containType.push(passUpper);
@@ -47,6 +60,7 @@ function getTypes() {
     containType.push(passUpper);
   }
 
+  //if character type numeric was confirmed, push array of numeric charatcers into arrGenerate and containType[2]=true
   if (charType[2] == true){
     arrGenerate.push(arrNum);
     containType.push(passNum);
@@ -55,6 +69,7 @@ function getTypes() {
     containType.push(passNum);
   }
 
+  //if character type special character was confirmed, push array of special charatcers into arrGenerate and containType[3]=true
   if (charType[3] == true){
     arrGenerate.push(arrSpecial);
     containType.push(passSpecial);
@@ -64,25 +79,25 @@ function getTypes() {
   }
 }
 
-var arrLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var arrUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var arrNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-var arrSpecial = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-
 var arrPassword = [];
 var stringPassword;
 function generatePassword(){
   var length = getLength();
   getTypes();
+  //arrays of what index the character type appears in from the randomly generated password array
   var numLower = [];
   var numUpper = [];
   var numNum = [];
   var numSpecial = [];
 
   arrPassword.length = length;
+  // for loop creates a password array
   for (var i = 0; i < arrPassword.length; i++){
+    // random index to pick from array arrGenerate that contains array of character types
     var randGenIndex = Math.floor(Math.random() * arrGenerate.length);
+    // array of a random character type from array arrGenerate
     var randArr = arrGenerate[randGenIndex];
+    // if and else if statements to get random index from the random character type array
     if (randArr.includes("a")){
       var charIndex = Math.floor(Math.random() * arrLower.length);
     }
@@ -95,8 +110,10 @@ function generatePassword(){
     else if (randArr.includes(" ")){
       var charIndex = Math.floor(Math.random() * arrSpecial.length);
     }
+    //adds random character into array arrPassword at index i
     arrPassword[i]=arrGenerate[randGenIndex][charIndex];
 
+    // if statements check to see which character type array arrPassword[i] is in and pushes i index from arrPassword into an array specific to the character type 
     if (arrLower.includes(arrPassword[i])) {
       numLower.push(i);
     }
@@ -111,21 +128,26 @@ function generatePassword(){
     }
   }
 
+  //if user critera contains lowercase and the randomly generated password does not contain lowercase
   if (containType[0]==true && numLower.length==0){
+    //if there is more than 1 uppercase in randomly generated password, randonly generate a lowercase charater to replace first index of uppercase which has index number of first appearance of uppercase in password array
     if (numUpper.length>1){
       var charIndex = Math.floor(Math.random() * arrLower.length);
       arrPassword[numUpper[0]]=arrLower[charIndex];
     }
+    //if there is more than 1 numeric in randomly generated password, randonly generate a lowercase charater to replace first index of numeric which has index number of first appearance of numeric in password array
     else if (numNum.length>1){
       var charIndex = Math.floor(Math.random() * arrLower.length);
       arrPassword[numNum[0]]=arrLower[charIndex];
     }
+    //if there is more than 1 special character in randomly generated password, randonly generate a lowercase charater to replace first index of special character which has index number of first appearance of special character in password array
     else if (numSpecial.length>1){
       var charIndex = Math.floor(Math.random() * arrLower.length);
       arrPassword[numSpecial[0]]=arrLower[charIndex];
     }
   }
 
+  //if user critera contains uppercase and the randomly generated password does not contain uppercase
   if (containType[1]==true && numUpper.length==0){
     if (numLower.length>1){
       var charIndex = Math.floor(Math.random() * arrUpper.length);
@@ -141,6 +163,7 @@ function generatePassword(){
     }
   }
 
+  //if user critera contains numeric charaters and the randomly generated password does not contain numeric characters
   if (containType[2]==true && numNum.length==0){
     if (numUpper.length>1){
       var charIndex = Math.floor(Math.random() * arrNum.length);
@@ -156,6 +179,7 @@ function generatePassword(){
     }
   }
 
+  //if user critera contains special charaters and the randomly generated password does not contain special characters
   if (containType[3]==true && numSpecial.length==0){
     if (numUpper.length>1){
       var charIndex = Math.floor(Math.random() * arrSpecial.length);
@@ -170,7 +194,7 @@ function generatePassword(){
       arrPassword[numLower[0]]=arrSpecial[charIndex];
     }
   }
-  
+  //makes array password into a string
   return stringPassword = arrPassword.join("");
 }
 
